213b4136c206cd4a075c6e16a500f2e611399e50
t2 <oo_ri@naver.com>
2021-03-14 03:39:34 +0900

    @expo/vector-icons and style update

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,11 +1,15 @@
 import React, { FC, memo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  temp: {
+    fontSize: 40,
   },
 });
 
@@ -17,7 +21,12 @@ interface IWeather {
 const Weather: FC<IWeather> = ({ condition, temp }) => {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
+      <View style={styles.container}>
+        <MaterialCommunityIcons name="weather-lightning-rainy" size={96} color="black" />
+        <Text style={styles.temp}>{temp}</Text>
+      </View>
+
+      <View style={styles.container}></View>
     </View>
   );
 };
