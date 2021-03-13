37176a79dd402ce72b209d01af100a5abe7e61d7
t2 <oo_ri@naver.com>
2021-03-13 21:10:27 +0900

    Expo Location Update

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import * as Location from 'expo-location';
 
 import { Loading } from './src/components';
 
 const App = () => {
-  return <Loading />;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await Location.requestPermissionsAsync();
+
+        const {
+          coords: { latitude, longitude },
+        } = await Location.getCurrentPositionAsync();
+
+        setIsLoading(false);
+      } catch (e) {
+        Alert.alert('Location Error', 'Current location could not be found.');
+      }
+    })();
+  }, []);
+
+  return isLoading ? <Loading /> : null;
 };
 
 export default memo(App);
