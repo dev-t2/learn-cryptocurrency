1e2289f881d270f4375fc24f9bc42ea5e1be37c4
t2 <oo_ri@naver.com>
2021-03-14 00:29:55 +0900

    Axios Install and Open Weather API Update

--- App.tsx
+++ App.tsx
@@ -1,26 +1,39 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { FC, memo, useCallback, useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 import * as Location from 'expo-location';
+import axios from 'axios';
 
+import { API_KEY } from './apiKey';
 import { Loading } from './src/components';
 
-const App = () => {
+const App: FC = () => {
   const [isLoading, setIsLoading] = useState(true);
 
+  const getWeather = useCallback(async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  }, []);
+
+  const getLocation = useCallback(async () => {
+    try {
+      await Location.requestPermissionsAsync();
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+
+      getWeather(latitude, longitude);
+
+      setIsLoading(false);
+    } catch (e) {
+      Alert.alert('Location Error', 'Current location could not be found.');
+    }
+  }, []);
+
   useEffect(() => {
-    (async () => {
-      try {
-        await Location.requestPermissionsAsync();
-
-        const {
-          coords: { latitude, longitude },
-        } = await Location.getCurrentPositionAsync();
-
-        setIsLoading(false);
-      } catch (e) {
-        Alert.alert('Location Error', 'Current location could not be found.');
-      }
-    })();
+    getLocation();
   }, []);
 
   return isLoading ? <Loading /> : null;
