52d690ad25d5cf4a1e35cf698b298ab4df039252
dev-t2 <oo_ri@naver.com>
2023-11-17 00:19:45 +0900

    02. Template HTML Partials Update

--- 02-template/main.go
+++ 02-template/main.go
@@ -14,16 +14,23 @@ type homeData struct {
 	Blocks []*blockchain.Block
 }
 
-const port = ":8080"
+const (
+	port = ":8080"
+	dir = "02-template/templates"
+)
+
+var templates *template.Template
 
 func home (w http.ResponseWriter, r *http.Request) {
-	html := template.Must(template.ParseFiles("02-template/templates/pages/home.html"))
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
 
-	html.Execute(w, data)
+	templates.ExecuteTemplate(w, "home", data)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(dir + "/pages/*.html"))
+	templates = template.Must(templates.ParseGlob(dir + "/partials/*.html"))
+
 	http.HandleFunc("/", home)
 
 	fmt.Printf("Listening on http://localhost%s\n", port)
