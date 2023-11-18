abd5bed91c31c2bcdfa7c9de0926886102a30fbb
dev-t2 <oo_ri@naver.com>
2023-11-18 20:18:32 +0900

    JSON Format Update

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/dev-t2/learn-cryptocurrency/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dev-t2/learn-cryptocurrency/utils"
+)
+
+const port = ":8080"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(res http.ResponseWriter, req *http.Request) {
+	data := []URLDescription{
+		{ URL: "/", Method: "GET", Description: "Get Documentation" },
+	}
+
+	b, err := json.Marshal(data)
+
+	utils.HandleErr(err)
+
+	fmt.Printf("%s\n", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+
+	fmt.Printf("Listening on http://localhost%s\n", port)
+
+	log.Fatal(http.ListenAndServe(port, nil))
 }
