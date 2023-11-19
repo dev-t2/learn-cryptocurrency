10d5bca870e5e7259a90da90a1780de9358e8835
dev-t2 <oo_ri@naver.com>
2023-11-19 14:15:30 +0900

    API Code Refactoring

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"github.com/dev-t2/learn-cryptocurrency/api"
 	"github.com/dev-t2/learn-cryptocurrency/explorer"
-	"github.com/dev-t2/learn-cryptocurrency/rest"
 )
 
 func main() {
-	go rest.Start(8080)
+	go api.Start(8080)
 
 	explorer.Start(8081)
 }
