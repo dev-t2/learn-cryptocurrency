e00c9873783ffaf424011d966d6c2ed24f125a4f
dev-t2 <oo_ri@naver.com>
2023-11-20 20:32:19 +0900

    CLI Parsing Arguments Update

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "github.com/dev-t2/learn-cryptocurrency/api"
+import (
+	"fmt"
+	"os"
+)
+
+func usage() {
+	fmt.Println("Please enter the following command.")
+	fmt.Println()
+	fmt.Println("explorer:  HTML Explorer")
+	fmt.Println("api:       REST API (Recommended)")
+	fmt.Println()
+
+	os.Exit(1)
+}
 
 func main() {
-	api.Start(8080)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start HTML Explorer")
+	case "api":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }
